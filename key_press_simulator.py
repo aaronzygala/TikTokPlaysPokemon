47ff704b5c0b079082089fa8a475f96248841a1b
aaronzygala <aaronzygala@gmail.com>
2023-08-25 17:26:01 -0400

    Made playsound non-blocking

--- key_press_simulator.py
+++ key_press_simulator.py
@@ -127,11 +127,11 @@ class KeyPressSimulator:
                 print("MUTING SONG...")
                 self.toggle_mute()
                 # Play the sound
-                playsound(constants.THEME_SONG)
+                playsound(constants.THEME_SONG, block=False)
                 print("THEME SONG COMPLETE")
 
                 # Wait for the sound to finish playing
-                #time.sleep(constants.THEME_SONG_DURATION_SECONDS)
+                time.sleep(constants.THEME_SONG_DURATION_SECONDS)
                 print("UN-MUTING SONG...")
                 self.toggle_mute()
             # Add more conditions for handling other sound requests
